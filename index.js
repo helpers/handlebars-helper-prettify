9f414c63d9b5ae82e2f3a4f9f20534d304b6c643
Jon Schlinkert <dev@sellside.com>
2013-09-02 12:21:02 -0400

    0.1.3

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var _ = require('lodash');
 
 module.exports.register = function(Handlebars, options) {
   var prettify = require('js-beautify').html;
+  var assembleOptions = options;
 
   var condense = function(str) {
     return str.replace(/(\n|\r){2,}/g, '\n');
@@ -22,18 +23,9 @@ module.exports.register = function(Handlebars, options) {
     return str.replace(/(<\/(a|span|strong|h1|h2|h3|h4|h5|h6)>(?!(,|\.|!|\?|;|:)))/g, '$1 ');
   };
 
-  /**
-   * Default options passed to js-beautify.
-   * @param {hash arguments} [Options received as hash arguments will override defaults.]
-   * @param {task options}   [Options defined in the task/target override hash arguments.]
-   */
-  var defaults = {};
-  var assembleOptions = options;
-  defaults = _.extend(assembleOptions.prettify, defaults);
-  defaults.indent_size = defaults.indent;
 
   /**
-   * Prettify HTML output
+   * Block helper for prettifying HTML output.
    * @example:
    *   {{#prettify indent="2"}}
    *     {{> body }}
@@ -44,16 +36,26 @@ module.exports.register = function(Handlebars, options) {
     var content = prettifyHTML(options.fn(this), hash);
 
     // Reduce multiple newlines to a single newline
-    if(assembleOptions.prettify.condense === true) {
+    if(hash.condense === true) {
       content = condense(content);
     }
     // Add a single newline above code comments.
-    if(assembleOptions.prettify.padcomments === true) {
+    if(hash.padcomments === true) {
       content = padcomments(content);
     }
-
     return fixspaces(content);
   });
+
+
+  /**
+   * Default options passed to js-beautify.
+   * @param {hash arguments} [Options received as hash arguments will override defaults.]
+   * @param {task options}   [Options defined in the task/target override hash arguments.]
+   */
+  var defaults = {};
+  defaults.indent_size = defaults.indent;
+  defaults = _.extend(assembleOptions.prettify, defaults);
+
 
   /**
    * Format HTML with js-beautify, pass in options.
@@ -62,13 +64,11 @@ module.exports.register = function(Handlebars, options) {
    * @returns {String}         [Stunning HTML.]
    */
   var prettifyHTML = function(source, options) {
-    options = options || {};
     try {
       return prettify(source, {
-      indent_size:       options.indent            || 2,
-      indent_char:       options.indent_char       || ' ',
-      indent_inner_html: options.indent_inner_html || true,
-      unformatted:       options.unformatted       || ['code', 'pre', 'em', 'strong']
+      indent_size: 2,
+      indent_inner_html: true,
+      unformatted: ['code', 'pre', 'em', 'strong']
     });
     } catch (e) {
       console.error(e);
@@ -76,4 +76,3 @@ module.exports.register = function(Handlebars, options) {
     }
   };
 };
-
